c747849a191f4d8a4d602f9b8a6d40c2413daa75
Stanislav Borisov <sdborisov@yahoo.com>
2017-01-29 20:23:53 +0200

    Add files via upload

---
+++ christmas/christmas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace christmas
+{
+    class christmas
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var star = n * 4 + 1;
+            var inner = 0;
+            var outer = n * 2 - 1;
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("{0}/|\\{0}", new string ('.',outer));
+
+            }
+            for(int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("{0}\\|/{0}", new string('.', outer));
+
+            }
+
+            for (int i = 0; i < n*2; i++)
+            {
+                Console.WriteLine("{0}*{1}*{1}*{0}", new string('.', outer), new string('-', inner));
+                outer -= 1;
+                inner += 1;
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(new string('*', n*4+1));
+            }
+            for (int i = 0; i < n*2; i++)
+            {
+                Console.Write("*.");
+
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("*");
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(new string('*', n * 4 + 1));
+            }
+
+
+        }
+    }
+}
